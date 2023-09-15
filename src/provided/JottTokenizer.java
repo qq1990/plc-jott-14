5e8b7985892e91fa8e5f3c14281feeb73c6e4f52
tse6329 <tse6329@g.rit.edu>
2023-09-14 20:13:32 -0400

    File Restructure and File IO

--- src/provided/JottTokenizer.java
+++ src/provided/JottTokenizer.java
@@ -1,22 +1,40 @@
 package src.provided;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 /**
  * This class is responsible for tokenizing Jott code.
  * 
- * @author Donald Burke, 
+ * @author Donald Burke, Thomas Ehlers
  **/
-
-import java.util.ArrayList;
-
 public class JottTokenizer {
-
-	/**
+    
+    /**
      * Takes in a filename and tokenizes that file into Tokens
      * based on the rules of the Jott Language
      * @param filename the name of the file to tokenize; can be relative or absolute path
      * @return an ArrayList of Jott Tokens
-     */
+    */
     public static ArrayList<Token> tokenize(String filename){
-		return null;
+        ArrayList<Token> tokenlist = new ArrayList<Token>();
+        String data = null;
+        try {
+            Scanner myReader = new Scanner(new File(filename));
+            myReader.useDelimiter("\\Z");
+            data = myReader.next();
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File "+filename+" could not be located/opened.");
+            //e.printStackTrace();
+        }
+        System.out.println(data);
+		return tokenlist;
 	}
+
+    public static void main(String[] args) {
+        tokenize("testCases\\tokenizerTestCases\\phase1Example.jott");
+    }
 }
