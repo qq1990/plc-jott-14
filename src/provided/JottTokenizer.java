4a8161ce44c7519c21de97c4c49ea47a84434f3b
Clarke Kennedy <clarkek0422@gmail.com>
2023-09-21 17:22:16 -0400

    added name

--- src/provided/JottTokenizer.java
+++ src/provided/JottTokenizer.java
@@ -8,7 +8,7 @@ import java.io.FileNotFoundException;
 /**
  * This class is responsible for tokenizing Jott code.
  * 
- * @author Donald Burke, Thomas Ehlers, Quan Quy
+ * @author Donald Burke, Thomas Ehlers, Quan Quy, Clarke Kennedy
  **/
 public class JottTokenizer {
     /**
