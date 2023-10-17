1b05fc7bc20e2f9be0da37d1cfce965877be625a
qq1990 <qq1990@g.rit.edu>
2023-10-17 09:43:11 -0400

    finish return and bodies - still need to fix dependent errors

---
+++ src/parser/BodyNode.java
@@ -0,0 +1,77 @@
+package src.parser;
+
+import java.util.ArrayList;
+import src.provided.JottTree;
+import src.provided.Token;
+import src.provided.TokenType;
+
+public class BodyNode implements JottTree {
+    private ArrayList<BodyStmtNode> bodyStmts;
+    private ReturnStmtNode returnStmt;
+
+    public BodyNode(ArrayList<BodyStmtNode> bodyStmts, ReturnStmtNode returnStmt) {
+        this.bodyStmts = bodyStmts;
+        this.returnStmt = returnStmt;
+    }
+
+    @Override
+    public String convertToJott() {
+        StringBuilder jottCode = new StringBuilder();
+        for (BodyStmtNode stmt : bodyStmts) {
+            jottCode.append(stmt.convertToJott());
+        }
+        if (returnStmt != null) {
+            jottCode.append(returnStmt.convertToJott());
+        }
+        return jottCode.toString();
+    }
+
+    @Override
+    public String convertToJava(String className) {
+        // TODO: Implement Java code generation for BodyNode
+        // You'll need to convert the body statements and return statement to Java code.
+        // Make sure to handle both cases where the body statements are present or not.
+        throw new UnsupportedOperationException("Unimplemented method 'convertToJava'");
+    }
+
+    @Override
+    public String convertToC() {
+        // TODO: Implement C code generation for BodyNode
+        // You'll need to convert the body statements and return statement to C code.
+        // Make sure to handle both cases where the body statements are present or not.
+        throw new UnsupportedOperationException("Unimplemented method 'convertToC'");
+    }
+
+    @Override
+    public String convertToPython() {
+        // TODO: Implement Python code generation for BodyNode
+        // You'll need to convert the body statements and return statement to Python code.
+        // Make sure to handle both cases where the body statements are present or not.
+        throw new UnsupportedOperationException("Unimplemented method 'convertToPython'");
+    }
+
+    @Override
+    public boolean validateTree() {
+        // TODO: Implement tree validation for BodyNode
+        // You can check if the body statements and return statement are valid here.
+        throw new UnsupportedOperationException("Unimplemented method 'validateTree'");
+    }
+
+    public static BodyNode parse(ArrayList<Token> tokens) throws SyntaxException {
+        ArrayList<BodyStmtNode> bodyStmts = new ArrayList<>();
+        ReturnStmtNode returnStmt = null;
+
+        // Parse body statements
+        while (!tokens.isEmpty()) {
+            BodyStmtNode bodyStmt = BodyStmtNode.parse(tokens);
+            if (bodyStmt != null) {
+                bodyStmts.add(bodyStmt);
+            } else {
+                throw new SyntaxException("Syntax Error in body node");
+            }
+        }
+
+        returnStmt = ReturnStmtNode.parse(tokens);
+        return new BodyNode(bodyStmts, returnStmt);
+    }
+}
---
+++ src/parser/BodyStmtNode.java
@@ -0,0 +1,50 @@
+package src.parser;
+import src.provided.JottTree;
+import src.provided.Token;
+import src.provided.TokenType;
+import java.util.ArrayList;
+
+public interface BodyStmtNode extends JottTree {
+    public static BodyStmtNode parse(ArrayList<Token> tokens) throws SyntaxException {
+        if (tokens.size() == 0) {
+            throw new SyntaxException("Syntax Error in BodyStmtNode");
+        }
+        Token t = tokens.get(0);
+
+        if (t.getToken().equals("if")) {
+            return IfNode.parse(tokens);
+        } else if (t.getToken().equals("while")) {
+            return WhileNode.parse(tokens);
+        }
+        else if (t.getTokenType() == TokenType.FC_HEADER){
+            FuncCallNode funcCall = FuncCallNode.parse(tokens);
+            if (tokens.get(0).getTokenType() == TokenType.SEMICOLON){
+                tokens.remove(0);
+                return funcCall;
+            }
+            else{
+                throw new SyntaxException("Error parsing function call");
+            }
+        }
+        else if (t.getTokenType() == TokenType.ID_KEYWORD) {
+            return AsmtNode.parse(tokens);
+            
+        }
+        else if ((t.getToken().equals("Double")
+            || t.getToken().equals("Integer")
+            || t.getToken().equals("String")
+            || t.getToken().equals("Boolean"))){
+            // Check for function calls or assignments
+            if (tokens.size() >= 3 && tokens.get(2).getTokenType() == TokenType.SEMICOLON) {
+                return VarDecNode.parse(tokens);
+            } else {
+                return AsmtNode.parse(tokens);
+            }
+        }
+        else{
+            throw new SyntaxException("Syntax Error in BodyStmtNode");
+        }
+    }
+
+    
+}
